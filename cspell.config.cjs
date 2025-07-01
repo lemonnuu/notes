877724048a29aa683970ae691d882655778a6ae5
junxinlei <junxinlei>
2025-07-01 19:14:31 +0800

    feat: 项目初始化

---
+++ cspell.config.cjs
@@ -0,0 +1,32 @@
+const { banWords } = require('cspell-ban-words');
+
+module.exports = {
+  version: '0.2',
+  language: 'en',
+  files: ['**/{*,.*}/**/*.{ts,tsx,js,jsx,md,mdx,yml,yaml}'],
+  enableFiletypes: ['mdx', 'github-actions-workflow'],
+  ignoreRegExpList: [
+    // ignore markdown anchors such as [fooBar](#foobar)
+    '#.*?\\)',
+  ],
+  ignorePaths: [
+    'dist',
+    'dist-*',
+    'compiled',
+    'coverage',
+    'doc_build',
+    'playwright-report',
+    'node_modules',
+    'lefthook.yml',
+    'pnpm-lock.yaml',
+  ],
+  flagWords: banWords,
+  dictionaries: ['dictionary'],
+  dictionaryDefinitions: [
+    {
+      name: 'dictionary',
+      path: './scripts/dictionary.txt',
+      addWords: true,
+    },
+  ],
+};
---
+++ packages/blog/rspress.config.ts
@@ -0,0 +1,21 @@
+import * as path from 'node:path';
+import { defineConfig } from 'rspress/config';
+
+export default defineConfig({
+  icon: '/rspress-icon.png',
+  logo: {
+    dark: '/rspress-dark-logo.png',
+    light: '/rspress-light-logo.png',
+  },
+  root: path.join(__dirname, 'docs'),
+  themeConfig: {
+    socialLinks: [
+      {
+        content: 'https://github.com/web-infra-dev/rspress',
+        icon: 'github',
+        mode: 'link',
+      },
+    ],
+  },
+  title: 'My Site',
+});
